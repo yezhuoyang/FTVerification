27f91e54ac371902e555d0c3e2c55069246aab67
John ye <34168020+yezhuoyang@users.noreply.github.com>
2025-03-03 14:46:49 -0800

    Sample from QEPG

--- QEPG.py
+++ QEPG.py
@@ -17,10 +17,11 @@ class QEPG:
         self._YerrorMatrix=np.zeros((3*self._total_noise, self._total_meas), dtype=int)
         self._ZerrorMatrix=np.zeros((3*self._total_noise, self._total_meas), dtype=int)
 
+
     def compute_graph(self):
         for i in range(self._total_noise):
             self._tracer.reset()
-            self._tracer.set_noise_type(1)
+            self._tracer.set_noise_type(i,1)
             self._tracer.prop_all()
             measured_error=self._tracer.get_measuredError()
             for j in range(self._total_meas):
@@ -31,7 +32,7 @@ class QEPG:
                 elif measured_error[j]=='Z':
                     self.add_z_type_edge(1,i,j)
             self._tracer.reset()    
-            self._tracer.set_noise_type(2)
+            self._tracer.set_noise_type(i,2)
             self._tracer.prop_all()
             measured_error=self._tracer.get_measuredError()
             for j in range(self._total_meas):
@@ -42,7 +43,7 @@ class QEPG:
                 elif measured_error[j]=='Z':
                     self.add_z_type_edge(2,i,j)
             self._tracer.reset()    
-            self._tracer.set_noise_type(3)
+            self._tracer.set_noise_type(i,3)
             self._tracer.prop_all()
             measured_error=self._tracer.get_measuredError()
             for j in range(self._total_meas):
@@ -115,3 +116,280 @@ class QEPG:
 
     def matrix(self):
         pass
+
+
+
+
+import random
+
+
+def sample_fixed_one_two_three(N, k):
+    """
+    Returns a list of length N containing exactly k ones 
+    (and N-k zeros), in a random order.
+    """
+    # Step 1: Create a list of k ones and N-k zeros
+    arr = [1]*k + [0]*(N-k)
+    
+    # Step 2: Create a list of 1 or two
+    arrtype=[]
+    
+    for i in range(N):
+        arrtype.append(random.randint(1,3))
+
+    
+    # Step 2: Shuffle the list randomly
+    random.shuffle(arr)
+    random.shuffle(arrtype)
+    
+    return [a * b for a, b in zip(arr, arrtype)]
+
+
+
+
+class NaiveSampler():
+    def __init__(self, circuit:CliffordCircuit):
+        self._qubit_num=circuit._qubit_num     
+        self._circuit=circuit
+        self._totalnoise=circuit.get_totalnoise()
+
+        self._logical_error_rate=0
+
+        self._dataqubits=None
+        self._syndromequbits=None
+        self._stimcircuit=None
+
+        self._shots=10
+
+    def set_shots(self, shots):
+        self._shots=shots
+
+
+    def calc_logical_error_rate(self):
+        self._stimcircuit=self._circuit.get_stim_circuit()
+
+        sampler = self._stimcircuit.compile_detector_sampler()
+       
+        detector_error_model = self._stimcircuit.detector_error_model(decompose_errors=True)
+
+        num_errors = 0
+
+        for i in range(self._shots):
+            detection_events, observable_flips = sampler.sample(1, separate_observables=True)
+            #detection_events, observable_flips = sampler.sample(self._shots, separate_observables=True)
+            # Configure a decoder using the circuit.
+            #print(detection_events)
+            #print(observable_flips)
+            
+            
+            matcher = pymatching.Matching.from_detector_error_model(detector_error_model)
+            predictions = matcher.decode_batch(detection_events)
+            #print(predictions)
+    
+        
+            actual_for_shot = observable_flips[0]
+            predicted_for_shot = predictions[0]
+            if not np.array_equal(actual_for_shot, predicted_for_shot):
+                num_errors += 1
+        
+        self._logical_error_rate=num_errors/self._shots
+
+        return self._logical_error_rate
+
+
+    
+
+
+
+class WSampler():
+    def __init__(self, circuit:CliffordCircuit):
+        self._inducedNoise=["I"]*circuit._qubit_num
+        self._measuredError={}
+        self._qubit_num=circuit._qubit_num     
+        self._circuit=circuit
+        self._totalnoise=circuit.get_totalnoise()
+
+        self._logical_error_distribution=[0]*self._totalnoise
+        self._logical_error_rate=0
+
+        
+        self._detection_events=[]
+        self._observable_flips=[]
+        self._dataqubits=None
+        self._syndromequbits=None
+        self._stimcircuit=None
+
+        self._detector_error_model=None
+        self._shots=10
+
+        self._noise_vector=np.array([0]*3*self._totalnoise)
+
+
+        self._QPEGraph=QEPG(circuit)
+        self._QPEGraph.compute_graph()
+
+
+    def set_shots(self, shots):
+        self._shots=shots
+
+
+    def set_dataqubits(self, dataqubits):
+        self._dataqubits=dataqubits
+
+    #Sample noise with weight K
+    def sample_noise(self,W):
+        random_index=sample_fixed_one_two_three(self._totalnoise,W)
+
+        self._noise_vector=np.array([0]*3*self._totalnoise)
+
+        for i in range(self._totalnoise):
+            if random_index[i]==1:
+                self._noise_vector[i]=1
+            elif random_index[i]==2:
+                self._noise_vector[i+self._totalnoise]=1
+            elif random_index[i]==3:
+                self._noise_vector[i+2*self._totalnoise]=1               
+
+
+    #Propagate the error, and get sample result
+    def calc_sample_result(self):
+        detectorresult=self._QPEGraph.sample_error(self._noise_vector) 
+
+
+        tmp_detection_events=[]
+        parity_group=self._circuit.get_parityMatchGroup()
+        for group in parity_group:
+            parity=0
+            for i in group:
+                if detectorresult[i]==1:
+                    parity+=1
+            parity=parity%2
+            if parity==1:
+                tmp_detection_events.append(True)
+            else:
+                tmp_detection_events.append(False)
+                
+        observable=self._circuit.get_observable()
+        tmp_observable_flips=[]
+        parity=0
+        for index in observable:
+            if detectorresult[index]==1:
+                parity+=1
+        parity=parity%2
+        if parity==1:
+            tmp_observable_flips.append(True)
+        else:
+            tmp_observable_flips.append(False)
+
+        return [tmp_detection_events], [tmp_observable_flips]
+         
+
+    def construct_detector_model(self):
+        self._stimcircuit=self._circuit.get_stim_circuit()
+        self._detector_error_model= self._stimcircuit.detector_error_model(decompose_errors=True)
+       
+
+
+    #Propagate the error, and return if there is a logical error
+    def has_logical_error(self):
+        # Configure a decoder using the circuit.
+        detection_events, observable_flips=self.calc_sample_result()
+        #print(detection_events)
+        #print(observable_flips)
+
+        matcher = pymatching.Matching.from_detector_error_model(self._detector_error_model)
+        predictions = matcher.decode_batch(detection_events)
+
+        if not np.array_equal(observable_flips[0],predictions[0]):
+            return True 
+        return False
+
+
+    def binomial_weight(self, W):
+        p=self._circuit._error_rate
+        N=self._totalnoise
+        if N<100:
+            return math.comb(N, W) * (p**W) * ((1 - p)**(N - W))
+        else:
+            lam = N * p
+            # PMF(X=W) = e^-lam * lam^W / W!
+            # Evaluate in logs to avoid overflow for large W, then exponentiate
+            log_pmf = (-lam) + W*math.log(lam) - math.lgamma(W+1)
+            return math.exp(log_pmf)
+
+    def calc_error_rate_with_fixed_weight(self, W):
+        errorshots=0
+        for i in range(self._shots):
+            self.sample_noise(W)
+            if self.has_logical_error():
+                errorshots+=1
+        self._logical_error_distribution[W]=errorshots/self._shots
+        #print(f"Sample done! W={W}")
+
+
+
+    def calc_logical_error_rate(self):
+        for i in range(self._totalnoise):
+            self.calc_error_rate_with_fixed_weight(i)
+            self._logical_error_rate+=self.binomial_weight(i)*self._logical_error_distribution[i]
+
+        return self._logical_error_rate
+
+
+
+if __name__ == "__main__":
+    '''
+    circuit=CliffordCircuit(2)
+    circuit.add_cnot(0,1)
+
+
+    circuit.add_measurement(0)
+    circuit.add_measurement(1)
+    print(circuit._totalnoise)
+
+
+    graph=QEPG(circuit)
+
+
+    graph.compute_graph()
+    print(graph._XerrorMatrix)
+    print(graph._YerrorMatrix)
+    print(graph._ZerrorMatrix)
+    '''
+
+
+    stim_circuit=stim.Circuit.generated("repetition_code:memory",rounds=1,distance=3).flattened()
+    stim_str=rewrite_stim_code(str(stim_circuit))
+
+    #print(stim_str)
+
+    circuit=CliffordCircuit(2)
+    circuit.set_error_rate(0.1)
+    circuit.compile_from_stim_circuit_str(stim_str)
+
+
+    #circuit_stim=circuit._stimcircuit
+
+
+    
+    Nsampler=NaiveSampler(circuit)
+    Nsampler.set_shots(10000)
+    Nsampler.calc_logical_error_rate()
+    print(Nsampler._logical_error_rate)
+
+    print("---------------------------------------------------------------")
+    #print(circuit._stimcircuit)
+
+
+    print(circuit._qubit_num)
+    
+    tracer=PauliTracer(circuit) 
+    sampler=WSampler(circuit)
+    sampler.set_shots(5000)
+    sampler.construct_detector_model()
+
+    sampler.calc_logical_error_rate()
+    print(sampler._logical_error_distribution)
+    print(sampler._logical_error_rate)
+    
+
